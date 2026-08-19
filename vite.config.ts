81b76c27fa7eca8d1c8bcd121ccef5ccf795ee94
pradneshrm <pradneshmohite110@gmail.com>
2026-08-19 09:22:54 +0530

    Change base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base: '/Portfolio-new/', // <-- ADD THIS LINE
+    base: '/pradnesh-mohite-portfolio/', // <-- ADD THIS LINE
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
